476fd2ebf5192deb11c999c4dc44984e53ce6861
ngxuancam <nguyenxuancamit@gmail.com>
2024-06-04 23:17:24 +0700

    first commit

---
+++ udagram-api-user/src/sequelize.ts
@@ -0,0 +1,19 @@
+import {Sequelize} from 'sequelize-typescript';
+import {config} from './config/config';
+
+
+export const sequelize = new Sequelize({
+  'username': config.username,
+  'password': config.password,
+  'database': config.database,
+  'host': config.host,
+  'port': 5432,
+  'dialect': config.dialect,
+  'storage': ':memory:',
+  dialectOptions: {
+    ssl: {
+      require: true, // Fix the ssl certificate when using AWS
+      rejectUnauthorized: false // Fix the ssl certificate when using AWS
+    }
+  },
+});
